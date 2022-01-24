87b24de6eb2a3f8c24bba40c26fc8728668b7ac5
cmin1226 <soojan62@gamil.com>
2022-01-25 03:02:14 +0900

    DB 공부 중

--- frontend/static/js/pages/login.js
+++ frontend/static/js/pages/login.js
@@ -5,18 +5,22 @@ export default class{
     async getHtml(){
         return `
             <form class="sign-form">
+                <div>
                 <input
                     required
                     type="text"
                     placeholder="아이디를 입력해주세요."
                 />
                 <input type="submit" value="Log In" />
+                </div>
+                <div>
                 <input
                     required
                     type="text"
                     placeholder="비밀번호를 입력해주세요."
                 />
                 <input type="submit" value="Password" />
+                </div>
             </form>
             <input type="submit" class="signup" value="회원가입" />
 
